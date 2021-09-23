e19fc171790ca97a5b7b04159c5a6cb0d02ca205
DmitriiPodgalo <podgalo32@icloud.com>
2021-09-23 12:53:54 +0300

    Converter is done

---
+++ units_converter.py
@@ -0,0 +1,14 @@
+from currency_converter import CurrencyConverter
+
+c = CurrencyConverter()
+
+while True:
+    try:
+        value = float(input('USD-RUB converter, please, enter the number value: '))
+    except ValueError:
+        print('Invalid inputs, please, try again!')
+    except:
+        print('Unknown error, please, try again!')
+    else:
+        break
+print(c.convert(value, 'USD', 'RUB'))
