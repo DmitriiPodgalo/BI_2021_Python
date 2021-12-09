aae503a063c29796e620a9bcbc6bd794e5c59bc2
DmitriiPodgalo <podgalo32@icloud.com>
2021-12-09 13:31:21 +0300

    flake8 fixed

--- sort.py
+++ sort.py
@@ -21,13 +21,13 @@ def main():
     else:
         args = parser()
         file = args.file
-        
+ 
         with open(file) as inf:
             for line in inf:
                 lines.append(line)
-    
+ 
     print(*sorted(lines), sep='', end='')
 
 
 main()
- 
+
